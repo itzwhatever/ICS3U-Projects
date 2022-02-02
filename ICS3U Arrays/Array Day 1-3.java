40414b3ad9a5e0a5d837cd8fc2054afbf5dfb007
Katrina <88742230+itzwhatever@users.noreply.github.com>
2022-02-02 13:42:34 -0500

    Create Array Day 1-3.java

---
+++ ICS3U Arrays/Array Day 1-3.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    int size;
+    Scanner input= new Scanner(System.in);
+    System.out.print("What is the size of the array?: ");
+    size=input.nextInt();
+    double[] values=new double[size];
+
+    
+
+
+  }
+}
