8fdaaf1d833dd6709b613819764e8e5218d933b6
Katrina <88742230+itzwhatever@users.noreply.github.com>
2022-02-02 13:44:18 -0500

    Create Array Day 1-4.java

---
+++ ICS3U Arrays/Array Day 1-4.java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main(String[] args) {
+  int[] a = { 1, 2, 3, 4, 5, 6, 7};
+  int[] b = new int[7];
+  
+  for (int i=0; i<a.length; i++){
+    b[i]=a[i];
+   }
+
+
+  }
+}
