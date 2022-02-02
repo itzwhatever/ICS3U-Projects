a3700aee6083231e7951f06c5ae530e40e2e329a
Katrina <88742230+itzwhatever@users.noreply.github.com>
2022-02-02 13:40:08 -0500

    Create Array Day 1-1.java

---
+++ ICS3U Arrays/Array Day 1-1.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+    int[] numbers1 = { 1, 3, 5, 9};
+    int[] numbers2 = { 1, 3, 5, 9};
+    int result;
+    // multiplying a index
+    result=numbers1[0]*numbers2[3];
+    System.out.println(result);
+  }
+}
