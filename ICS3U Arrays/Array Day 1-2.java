a406f60786d84b04be4fb29147a6b7d094d7a986
Katrina <88742230+itzwhatever@users.noreply.github.com>
2022-02-02 13:41:57 -0500

    Create Array Day 1-2.java

---
+++ ICS3U Arrays/Array Day 1-2.java
@@ -0,0 +1,13 @@
+class Main {
+  public static void main(String[] args) {
+    int[] array= new int[6];
+
+    for (int i=0; i< array.length; i++){
+      array[i]=-1;
+      System.out.println(array[i]);
+
+    }
+
+ 
+  }
+}
